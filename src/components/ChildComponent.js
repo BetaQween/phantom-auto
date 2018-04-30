a7d4dc7fb3295de7ac9a2d534a288f73232c6e34
BetaQween <beta.qween@gmail.com>
2018-04-30 15:41:35 +0300

    Passing to props object

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -3,26 +3,34 @@ import React, { Component } from 'react';
 class ChildComponent extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      newStaticData: +this.props.static
-    };
-    this.getDataChange = this.getDataChange.bind(this);
+    // this.state = {
+    //   newStaticData: +this.props.static
+    // };
+    // this.getDataChange = this.getDataChange.bind(this);
+    this.setStaticData = this.setStaticData.bind(this);
   }
-  componentWillReceiveProps(){
-    this.setState({newStaticData: this.state.newStaticData + 1})
-    }
-  getDataChange(){
-    console.log(this.props.dynamic.age);
+  // componentWillReceiveProps(){
+  //   this.setState({newStaticData: this.state.newStaticData + 1})
+  //   }
+  // getDataChange(){
+  //   console.log(this.props.dynamic.age);
+  // }
+  setStaticData(){
+    console.log(this.props.static.aa+1);
+  }
+  setDynamicData(){
+    console.log(this.props.dynamic.a-1);
   }
 
   render() {
-    if(this.props.dynamic){
-      this.getDataChange();
-    }
+    // if(this.props.dynamic){
+    //   this.getDataChange();
+    // }
     return (
       <div className="ChildComponent">
         ChildComponent:
-        <p>{this.props.dynamic.name}-{this.state.newStaticData}</p>
+        {/* <p>{this.props.dynamic.name}-{this.state.newStaticData}</p> */}
+        <p>{this.props.dynamic.a}-{this.props.static.aa}</p>
       </div>
     );
   }
--- src/components/ComponentTest.js
+++ src/components/ComponentTest.js
@@ -5,35 +5,47 @@ class ComponentTest extends Component {
 
   constructor(){
     super();
-    this.state = {
-      dynamicData: {name:1, age:2, year:3},
-      staticData: 22
-    };
+    // this.state = {
+    //   dynamicData: {name:1, age:2, year:3},
+    //   staticData: 22
+    // };
+    this.dynamicData = {a:1, b:2, c:3};
+    this.staticData = {aa:0, bb:1, cc:2};
     this.tick = this.tick.bind(this);
   }
 
   tick() {
-  var {name, age} = this.state.dynamicData;
-  this.setState(prevState => ({
-    dynamicData: {
-    ...prevState.dynamicData,
-    name: name + 1,
-    age: age - 1
-    }
-  }))
+  //   this.dynamicData.a++;
+  // var {name, age} = this.state.dynamicData;
+  // this.setState(prevState => ({
+  //   dynamicData: {
+  //   ...prevState.dynamicData,
+  //   name: name + 1,
+  //   age: age - 1
+  //   }
+  // }))
   }
-componentDidMount(){
-      setInterval(this.tick, 1000);
+componentWillReceiveProps(){
+  this.staticData.aa
+      setInterval(this.child.setStaticData(), 1000);
+      setInterval(this.child.setDynamicData(), 2000);
 }
 
 
   render() {
     return (
       <div className="ComponentTest">
+        {/* <p>{this.dynamicData.a}</p>
+        <p>{this.staticData.aa}</p> */}
         {/* <p>{this.state.dynamicData.name}</p>
         <p>{this.state.dynamicData.age}</p>
         <p>{this.state.dynamicData.year}</p> */}
-        <ChildComponent dynamic={this.state.dynamicData} static={this.state.staticData}/>
+        {/* <ChildComponent dynamic={this.state.dynamicData} static={this.state.staticData}/> */}
+        <ChildComponent
+           dynamic={this.dynamicData}
+           static={this.staticData}
+           ref={instance => {this.child = instance}}
+         />
       </div>
     );
   }
